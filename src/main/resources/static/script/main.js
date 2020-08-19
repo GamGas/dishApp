777c492f4f1ae11d7b7d2aa763ccfbf424d76672
Gamid Gasanov <gasanov.gamid.1993@gmail.com>
2020-08-19 19:22:53 +0300

    реализована живая подстановка тегов при поиске блюд. получает данные из БД

--- src/main/resources/static/script/main.js
+++ src/main/resources/static/script/main.js
@@ -1,7 +1,98 @@
-var app = angular.module("UserItemManagement", []);
+var app = angular
+    .module("UserItemManagement",
+        ['ngMaterial',
+            'ngMessages',
+            'material.svgAssetsCache'])
+    .directive('mdChips', function ($timeout) {
+        return {
+            restrict: 'E',
+            require: 'mdChips',
+            link: function (scope, element, attributes, mdChipsCtrl) {
+                var mouseUpActions = [];
+                mdChipsCtrl.onInputBlur = function (event) {
+                    this.inputHasFocus = false;
+                    mouseUpActions.push((function () {
+                        var chipBuffer = this.getChipBuffer();
+                        if (chipBuffer !== "") {
+                            this.appendChip(chipBuffer);
+                            this.resetChipBuffer();
+                        }
+                    }).bind(this));
+                };
+                window.addEventListener('click', function (event) {
+                    while (mouseUpActions.length > 0) {
+                        var action = mouseUpActions.splice(0, 1)[0];
+                        $timeout(function () {
+                            $timeout(action);
+                        });
+                    }
+                }, false);
+            }
+        }
+    });
 
 //Controller part
 app.controller("UserItemController", function ($scope, $http) {
+    var self = this;
+    self.readonly = false;
+    self.selectedItem = null;
+    self.searchText = null;
+    self.querySearch = querySearch;
+    self.productsList = [];
+    self.selectedProducts = [];
+    self.numberChips = [];
+    self.numberChips2 = [];
+    self.numberBuffer = '';
+    self.autocompleteRequireMatch = false;
+    self.transformChip = transformChip;
+    _loadProductList()
+    self.notFound = function (key) {
+        transformChip(key);
+    }
+
+    function transformChip(chip) {
+        if (angular.isObject(chip)) {
+            return chip;
+        }
+        return {
+            id: 1,
+            title: chip
+        }
+    }
+
+    function querySearch(query) {
+        return query
+            ? self.productsList.filter(createFilterFor(query))
+            : [];
+    }
+
+    function createFilterFor(query) {
+        var lowercaseQuery = angular.lowercase(query);
+        return function filterFn(product) {
+            return (product._lowertitle.indexOf(lowercaseQuery) === 0);
+        }
+    }
+
+    function _loadProductList() {
+        $http({
+            method: 'GET',
+            url: '/products'
+        }).then(
+            function (res) { // success
+                self.productsList = res.data.productsList;
+                self.productsList.map(function (prod) {
+
+                    prod._lowertitle = prod.title.toLowerCase();
+
+                    return prod;
+                });
+            },
+            function (res) { // error
+                console.log("Error: " + res.status + " : " + res.data);
+            }
+        );
+    }
+
 
     $scope.userItems = [];
     $scope.useritemForm = {
@@ -59,6 +150,7 @@ app.controller("UserItemController", function ($scope, $http) {
         $scope.useritemForm.password = userItem.password;
     };
 
+
     // Private Method
     // HTTP GET- get all employees collection
     // Call: http://localhost:8080/employees
@@ -96,3 +188,4 @@ app.controller("UserItemController", function ($scope, $http) {
         $scope.useritemForm.password = "";
     };
 });
+
